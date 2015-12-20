0bdccd633a8cf6b9eccc8cfe783e4f0f00fd0eba
Cédric <cedced19@gmail.com>
2015-12-20 12:18:53 +0000

    Add untracked files

---
+++ public/javascripts/controllers/signup.js
@@ -0,0 +1,15 @@
+module.exports = ['$scope', '$location', '$http', '$rootScope', 'sweet', function($scope, $location, $http, $rootScope, sweet) {
+        $rootScope.nav = '';
+   
+        $scope.signup = function () {
+            $http.post('/api/registrants', {
+                email: $scope.email,
+                password: $scope.password
+            }).success(function() {
+                $location.path('/');
+                sweet.show('You have been registered.', '', 'success');
+            }).error(function() {
+                sweet.show('Oops...', 'Something went wrong!', 'error');
+            });
+        };
+}];
