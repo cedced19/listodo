f32b0dd930aa5b768a7fb647797d1e7baeb42aa0
Cédric <cedced19@gmail.com>
2016-05-21 18:32:57 +0200

    Remove semver dependencie from client

--- routes/version-api.js
+++ routes/version-api.js
@@ -2,14 +2,21 @@ var express = require('express');
 var router = express.Router();
 var pkg = require('../package.json');
 var checkUpdate = require('check-update');
+var auth = require('../policies/auth.js');
+var semver = require('semver');
 
 /* GET Version */
-router.get('/', function(req, res, next) {
+router.get('/', auth, function(req, res, next) {
     checkUpdate({packageName: pkg.name}, function(err, latestVersion){
       if(err) return next(err);
+      var update = false;
+      if (semver.lt(pkg.version, latestVersion)) {
+        update = true;
+      }
       res.json({
           local: pkg.version,
-          github: latestVersion,
+          last: latestVersion,
+          update: update,
           url: pkg.homepage + '/releases/latest'
       });
     });
